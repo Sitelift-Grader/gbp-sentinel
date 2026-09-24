e15e6b3c67961d15cd5b27f4f17e3166c828c583
GBP Sentinel Contributors <sitelift-grader@users.noreply.github.com>
2026-09-24 20:00:27 +0200

    chore: ag auto-sync

---
+++ gbp_sentinel/detectors/__init__.py
@@ -0,0 +1,13 @@
+from .policy_detectors import (
+    KeywordStuffingDetector,
+    VirtualOfficeDetector,
+    DuplicateDetector,
+    LeadGenDetector,
+)
+
+__all__ = [
+    "KeywordStuffingDetector",
+    "VirtualOfficeDetector",
+    "DuplicateDetector",
+    "LeadGenDetector",
+]
